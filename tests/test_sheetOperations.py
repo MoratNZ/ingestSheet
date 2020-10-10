6ceff18e19a15e60112119a632a5d2b31ccee54d
David Maclagan <dmaclagan@teamtalk.co.nz>
2020-10-10 20:19:13 +1300

    Fixes typo
    python uses continue, not next

--- tests/test_sheetOperations.py
+++ tests/test_sheetOperations.py
@@ -29,7 +29,7 @@ class TestSheetOperations(unittest.TestCase):
         result = sheetOperations.parseSheet(sheet, headerRowCount=2)
 
         self.assertDictEqual(result, {'row one': {'parent one': {'child one': 1}, 'parent two': {'child one': 3, 'child two': 6, 'child three': 9}, 'parent three': {'child one': 11, 'child two': 14}, 'no parent': 17, 'no children': 20}, 'row two': {'parent one': {'child one': 2}, 'parent two': {'child one': 4, 'child two': 7, 'child three': None}, 'parent three': {'child one': 12, 'child two': 15}, 'no parent': 18, 'no children': 21}, 'row three': {
-                             'parent one': {'child one': None}, 'parent two': {'child one': 5, 'child two': 7, 'child three': 10}, 'parent three': {'child one': 13, 'child two': 16}, 'no parent': 19, 'no children': 22}, None: {'parent one': {'child one': None}, 'parent two': {'child one': None, 'child two': None, 'child three': None}, 'parent three': {'child one': None, 'child two': None}, 'no parent': None, 'no children': None}})
+                             'parent one': {'child one': None}, 'parent two': {'child one': 5, 'child two': 7, 'child three': 10}, 'parent three': {'child one': 13, 'child two': 16}, 'no parent': 19, 'no children': 22}})
 
 
 if __name__ == '__main__':
