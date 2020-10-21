c7285b000b4514e4b4c23bd100bc958faf235e7e
David Maclagan <dmaclagan@teamtalk.co.nz>
2020-10-21 14:41:48 +1300

    Adds check for empty header array

--- ingestSheet/rowOperations.py
+++ ingestSheet/rowOperations.py
@@ -5,7 +5,7 @@ def parseRow(headerArray, rowarray):
     rowDict = {}
 
     for cellIndex in range(len(headerArray)):
-        if headerArray[cellIndex] is not None:
+        if headerArray[cellIndex] is not None and len(headerArray[cellIndex] > 0):
             insertIntoDict(
                 rowDict, headerArray[cellIndex], rowarray[cellIndex].value)
 
